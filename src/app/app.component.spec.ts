6f107e6cdc3a7f0dcaac4905b1f41223e8567854
Reethika Shetty <shettyreethika31@gmail.com>
2021-03-12 14:09:58 +0530

    build fail test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TextProj1'`, () => {
+  it(`should have as title 'TestProj1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('TextProj1');
