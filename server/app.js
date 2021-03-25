6f0ea890b78b91f624e896ed04487183c1c61c5c
Reethika Shetty <shettyreethika31@gmail.com>
2021-03-25 09:49:02 +0530

    Added node server

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+});
+
+app.listen(4000, ()=> console.log("Listening on port 4000!"))
